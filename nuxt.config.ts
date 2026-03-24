3d4cfe1a4cc15405fbde181f036116bd153dce4e
Николай Жарников <zharnikovnikolay@yandex.ru>
2026-03-25 03:13:02 +1000

    fix: v0.2, hotfix config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   app: {
+    baseURL: '/zondermainy_cv/',
     head: {
       title: 'Nikolay Zharnikov — DevOps Engineer',
       meta: [
